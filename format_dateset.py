86325324cdb4b34a922323d9f7f686da1839d246
edmundwsy <edmundwsy@outlook.com>
2022-05-02 23:20:11 +0200

    update new file structure to suit requirement

--- format_dateset.py
+++ format_dateset.py
@@ -20,33 +20,47 @@
 """
 import os
 import glob
+import re
 
 
-def unzip_files(path, string="zurich_city"):
+def unzip_files(path, directory, string="zurich_city"):
     """
     unzip files and save to new directory with format
     """
-    no_replace = string.replace("_", "/")
-    unzipped_dir = path.replace(".zip", "")
-    new_dir = unzipped_dir.replace("_", "/")
-    new_dir = new_dir.replace(no_replace, string)
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
-        os.system("unzip " + path + " -d " + new_dir)
-    print("File unzipped and saved to: " + new_dir)
+   
+    filename = path.replace(".zip", "").split("/")[-1]
+    old_dir = path.replace(".zip", "").replace(filename, "")
+    
+    # match "zurich_city_11_a"    
+    z = re.match(string + "_\d+_\w", filename)
+    print(z.group())
+    new_dir = z.group()
+    rest = filename.replace(new_dir, "").replace("_", "/")
+    
+    save_dir = directory + new_dir + rest
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+        os.system("unzip " + path + " -d " + save_dir)
+    print("File unzipped and saved to: " + save_dir)
 
 
-def copy_files(path, string="zurich_city"):
+def copy_files(path, directory, string="zurich_city"):
+
     filename = path.split("/")[-1]
-    no_replace = string.replace("_", "/")
-    new_path = path.replace("_", "/")
-    new_path = new_path.replace(no_replace, string)
-    print(new_path)
+    old_dir = path.replace(filename, "")
+    
+    # match "zurich_city_11_a"    
+    z = re.match(string + "_\d+_\w", filename)
+    print(z.group())
+    new_dir = z.group()
+    rest = filename.replace(new_dir, "").replace("_", "/")
+    
+    save_dir = directory + new_dir + rest
+    
+    os.system("cp " + path + " " + save_dir)
+    print("File saved to: " + save_dir)
 
-    new_dir = new_path.replace(filename, "")
-    print(new_dir)
-    print("File saved to: " + new_dir)
-    os.system("mv " + path + " " + new_dir)
 
 
 if __name__ == "__main__":
@@ -54,7 +68,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--zip_dir", default="/home/siyuan/Downloads/", help="Path to zip files")
-    # parser.add_argument('--save_dir', default="/home/siyuan/workspace/CVbyDL/train", help='Path to save DSEC dataset directory')
+    parser.add_argument('--save_dir', default="/home/siyuan/workspace/CVbyDL/train/", help='Path to save DSEC dataset directory')
     parser.add_argument("--string", default="zurich_city", help="String of the data set")
     args = parser.parse_args()
 
@@ -62,8 +76,9 @@ if __name__ == "__main__":
     print(file_path_list)
 
     for file in file_path_list:
+        print()
         if "zip" in file:
-            unzip_files(file, args.string)
+            unzip_files(file, args.save_dir, args.string)
 
         else:
-            copy_files(file, args.string)
+            copy_files(file, args.save_dir, args.string)
